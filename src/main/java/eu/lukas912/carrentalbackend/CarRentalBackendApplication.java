f10209610595a085138a80898d76b8be4d189535
lukas nowy <lukas.n912@gmail.com>
2021-12-20 02:20:24 +0100

    Spring Project created

---
+++ src/main/java/eu/lukas912/carrentalbackend/CarRentalBackendApplication.java
@@ -0,0 +1,13 @@
+package eu.lukas912.carrentalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/lukas912/carrentalbackend/CarRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.lukas912.carrentalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
